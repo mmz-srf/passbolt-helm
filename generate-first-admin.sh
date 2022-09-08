97286baa65f633d24e84c9bdb3f7e726127a6bb7
Jean-Christophe Vassort <anatomicjc@open-web.fr>
2022-09-08 17:36:26 +0200

    Add helper scripts

---
+++ generate-first-admin.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ ! -f variables.env ]
+then
+  cp variables.env.sample variables.env
+fi
+
+. variables.env
+
+kubectl exec -it \
+  $(kubectl get po --no-headers -l app.kubernetes.io/name=passbolt-helm | awk '{print $1}') \
+  -- su -m -c "bin/cake passbolt register_user \
+  -u ${FIRST_ADMIN_EMAIL} \
+  -f "${FIRST_ADMIN_NAME}" \
+  -l "${FIRST_ADMIN_SURNAME}" -r admin" -s /bin/sh www-data
